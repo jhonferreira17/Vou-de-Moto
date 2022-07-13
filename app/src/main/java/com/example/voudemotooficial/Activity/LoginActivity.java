0a3113d0b3b6f189fedfb26377b5b34f00d26e3c
natasharhayane <rhayane.pereira3432@gmail.com>
2022-07-12 21:08:40 -0400

    Conexão com API de geolocalização e tratamento do login

--- app/src/main/java/com/example/voudemotooficial/Activity/LoginActivity.java
+++ app/src/main/java/com/example/voudemotooficial/Activity/LoginActivity.java
@@ -7,9 +7,12 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.voudemotooficial.Model.Usuario;
 import com.example.voudemotooficial.databinding.ActivityCadastroMototaxiBinding;
 import com.example.voudemotooficial.databinding.ActivityLoginBinding;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -41,7 +44,10 @@ public class LoginActivity extends AppCompatActivity {
         if(!email.isEmpty()){
             if(!senha.isEmpty()){
                 binding.progressBar.setVisibility(View.VISIBLE);
-               loginFirebase(email, senha);
+                Usuario usuario = new Usuario();
+                usuario.setEmail( email);
+                usuario.setSenha( senha);
+               loginFirebase(usuario);
             }else{
                 Toast.makeText(this, "Informe a sua senha", Toast.LENGTH_LONG).show();
             }
@@ -50,16 +56,27 @@ public class LoginActivity extends AppCompatActivity {
         }
     }
 
-    private void loginFirebase(String email, String senha){
+    private void loginFirebase(Usuario usuario){
         mAuth.signInWithEmailAndPassword(
-                email, senha).addOnCompleteListener(task -> {
+                usuario.getEmail(), usuario.getSenha()).addOnCompleteListener(task -> {
 
             if(task.isSuccessful()){
                 finish();
-                startActivity(new Intent(this, MainActivity.class));
+                startActivity(new Intent(this, MapsActivityPassageiro.class));
             }else{
                 binding.progressBar.setVisibility(View.GONE);
-                Toast.makeText(this, "Ocorreu um erro", Toast.LENGTH_LONG).show();
+                String excecao = "";
+                try {
+                    throw task.getException();
+                }catch ( FirebaseAuthInvalidUserException e ) {
+                    excecao = "Usuário não está cadastrado.";
+                }catch ( FirebaseAuthInvalidCredentialsException e ){
+                    excecao = "E-mail e senha não correspondem a um usuário cadastrado";
+                }catch (Exception e){
+                    excecao = "Erro ao cadastrar usuário: "  + e.getMessage();
+                    e.printStackTrace();
+                }
+                Toast.makeText(LoginActivity.this, excecao, Toast.LENGTH_LONG).show();
             }
         });
     }
--- app/src/main/java/com/example/voudemotooficial/Config/Config.java
+++ app/src/main/java/com/example/voudemotooficial/Config/Config.java
@@ -1,4 +1,30 @@
 package com.example.voudemotooficial.Config;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class Config {
+
+    private static DatabaseReference database;
+    private static FirebaseAuth auth;
+
+    //retorna a instancia do FirebaseDatabase
+    public static DatabaseReference getFirebaseDatabase(){
+
+        if ( database == null ){
+            database = FirebaseDatabase.getInstance().getReference();
+        }
+        return database;
+    }
+
+    //retorna a instancia do FirebaseAuth
+    public static FirebaseAuth getFirebaseAutenticacao(){
+
+        if( auth == null ){
+            auth = FirebaseAuth.getInstance();
+        }
+        return auth;
+    }
 }
+
